5ba4472b3f03a82c9b6e847a0be89f25b43636c9
sahariardev <sahariaralamk@gmail.com>
2018-11-26 23:30:53 +0600

    error fixed

--- employee/Models/target.py
+++ employee/Models/target.py
@@ -5,7 +5,7 @@ class Target(models.Model):
     employee=models.ForeignKey(Employee,on_delete=models.CASCADE)
     description=models.TextField(null=False)
     points=models.TextField(null=False)
-    status=models.CharField(null=False)
+    status=models.CharField(null=False,max_length=10)
 
     def __str__(self):
         return self.employee.name+" "+self.description
