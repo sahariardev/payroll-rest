dbab8d36ca3cb5b6b339e404c84231007f0e1c2e
sahariardev <sahariaralamk@gmail.com>
2018-12-03 23:43:47 +0600

    unnecessary files deleted

--- employee/Models/Type.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Type(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(null=False)
-
-    def __str__(self):
-        return self.name
--- employee/admin.py
+++ employee/admin.py
@@ -1,26 +1,5 @@
 from django.contrib import admin
 
-from .Models.Package import Package
-from .Models.Type import Type
-from .Models.Employee import Employee
-from .Models.Attendance import  Attendance
-from .Models.bonus import Bonus
-from .Models.GrantLeave import GrantLeave
-from .Models.Advance import  Advance
-from .Models.Payment import Payment
-from .Models.Target import Target
-from .Models.WorkedHours import WorkedHours
 
-# Register your models here.
-admin.site.register(Package)  # Salary Package
-admin.site.register(Type)   # Employee Type
-admin.site.register(Employee)   # Employee Entity
-admin.site.register(Attendance)
-admin.site.register(Bonus)
-admin.site.register(GrantLeave)
-admin.site.register(Advance)
-admin.site.register(Payment)
-admin.site.register(Target)
-admin.site.register(WorkedHours)
 
 
