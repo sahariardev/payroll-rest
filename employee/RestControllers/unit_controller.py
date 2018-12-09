fa5967570c9bdeba961288238c1ddd7c46a22a73
sahariardev <sahariaralamk@gmail.com>
2018-12-09 23:12:51 +0600

    UnitList View added

--- employee/RestControllers/unit_controller.py
+++ employee/RestControllers/unit_controller.py
@@ -1,3 +1,9 @@
 from rest_framework.generics import ListAPIView,CreateAPIView,UpdateAPIView,RetrieveAPIView
+from rest_framework import  renderers
 from ..Models.unit import Unit
+from ..Serializers.unit_serializer import UnitListSerializer
 
+class UnitListView(ListAPIView):
+    queryset = Unit.objects.all()
+    serializer_class = UnitListSerializer
+    renderer_classes = [renderers.JSONRenderer]
