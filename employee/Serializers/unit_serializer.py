b3b6fcf286bf9555b417610aa6d0ca8b19381e93
sahariardev <sahariaralamk@gmail.com>
2018-12-09 23:10:02 +0600

    All serializers for unit added

--- employee/Serializers/unit_serializer.py
+++ employee/Serializers/unit_serializer.py
@@ -1,7 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from ..Models.unit import Unit
 
+
 class UnitListSerializer(ModelSerializer):
     class Meta:
         model=Unit
         fields=['name','symbol'];
+
+
+'''This serializer will be used for detail, create and update'''
+
+
+class UnitDetailSerializer(ModelSerializer):
+    class Meta:
+        model=Unit
+        fields=['name','symbol','description'];
+
+
