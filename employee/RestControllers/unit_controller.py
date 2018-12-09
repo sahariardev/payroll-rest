f103767f87cb3ec486fe790dc8bd25fd3d31191e
sahariardev <sahariaralamk@gmail.com>
2018-12-09 23:15:31 +0600

    UnitDetail view added

--- employee/RestControllers/unit_controller.py
+++ employee/RestControllers/unit_controller.py
@@ -1,9 +1,17 @@
 from rest_framework.generics import ListAPIView,CreateAPIView,UpdateAPIView,RetrieveAPIView
 from rest_framework import  renderers
 from ..Models.unit import Unit
-from ..Serializers.unit_serializer import UnitListSerializer
+from ..Serializers.unit_serializer import UnitListSerializer,UnitDetailSerializer
+
 
 class UnitListView(ListAPIView):
     queryset = Unit.objects.all()
     serializer_class = UnitListSerializer
+    renderer_classes = [renderers.JSONRenderer]
+
+
+class UnitDetailView(RetrieveAPIView):
+    queryset = Unit.objects.all()
+    serializer_class = UnitDetailSerializer
+    lookup_field = 'pk'
     renderer_classes = [renderers.JSONRenderer]
