ecc943331aec65df7e772c5b02a9b9bb4356fe7a
sahariardev <sahariaralamk@gmail.com>
2018-12-17 23:04:36 +0600

    CORS added

--- rest/settings.py
+++ rest/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'employee',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
