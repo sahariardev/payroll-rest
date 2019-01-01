bdc7b6a011ff9e6647188f62fa7ef4d0e88fa655
sahariardev <sahariaralamk@gmail.com>
2019-01-01 22:30:08 +0600

    employee get from request added

--- employee/RestControllers/salary_calculation_controller.py
+++ employee/RestControllers/salary_calculation_controller.py
@@ -24,8 +24,8 @@ class SalaryCalculationTestView(APIView):
 
         print(employee)
 
-        #salary_details=SalaryDetail.objects.all().filter( effective_from__gte = from_date).filter(effective_till_date__lt=till_date).filter(employee__id=employee)
-        salary_details = SalaryDetail.objects.all().filter(employee__id=employee)
+        salary_details=SalaryDetail.objects.all().filter( effective_from__gte = from_date).filter(employee__id=employee)
+        #salary_details = SalaryDetail.objects.all().filter(employee__id=employee)
         serializer= SalaryDetailListSerializer(salary_details,many=True)
         print(serializer.data)
         return Response(serializer.data,status=status.HTTP_201_CREATED)
